ee6084b246cee0be6dfce6ecd0721ec5ddcf4cb0
Aaron Crane <skilledjuggler@gmail.com>
2020-12-01 16:34:38 -0800

    added class to extract database calls

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const morgan = require("morgan");
 const axios = require("axios");
 const dbquery = require("./lib/db-query.js");
-const buildRequestResponse = require("./lib/buildRequestResponse.js");
+// const buildRequestResponse = require("./lib/buildRequestResponse.js");
+const pgPersistance = require("./lib/pg-persistance.js");
+const DatabaseInterval = require("./lib/database-interval.js");
+
+new DatabaseInterval();
 
 const app = express();
 const port = 3001;
@@ -13,7 +17,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.locals.user = new pgPersistance();
+  next();
+})
+
 app.options("*", cors());
+
+
 
 async function checkDatabaseForFutureRequests() {
   let result = await dbquery("SELECT * FROM requests WHERE raw_response IS NULL ORDER BY time_scheduled");
@@ -38,20 +49,6 @@ function createTimeScheduled(userRequest) {
 
   return `${date} ${time} ${timeZone}`;
 }
-
-// (
-  // async function sendFutureRequest() {
-  // let futureRequests = await checkDatabaseForFutureRequests();
-  // for (let i = 0; i < futureRequests.length; i++) {
-  //   let request = futureRequests[i];
-
-  //   let options = generateRequestOptions(request.user_request);
-  //   let responseData = await axios(options);
-  //   await insertRawRequestResponse(responseData, request.id);
-  // }
-
-// setInterval(async () => {}, 1000 * 60);)();
-
 
 setInterval(async () => {
   let futureRequests = await checkDatabaseForFutureRequests();
@@ -95,19 +92,11 @@ function generateRequestOptions(userRequest) {
 
 async function sendRequest(userRequest, newlyCreatedRequestId, res) {
   try {
-    // if NOW is later than or equal the time of userRequest 
-      // send it now and do the next insert
-      // '2020-11-30 13:30 PST'
-
       let timeNow = new Date();
       let scheduledTime = new Date(`${userRequest.date} ${userRequest.time} ${userRequest.timeZone}`);
 
-      // console.log(timeNow, scheduledTime, "line 105");
-      // console.log(timeNow > scheduledTime, "line 106");
-
       if (timeNow >= scheduledTime) {
         let options = generateRequestOptions(userRequest);
-        console.log("3. Server sends API call on behalf of users", Date.now())
         let responseData = await axios(options);
         await insertRawRequestResponse(responseData, newlyCreatedRequestId);
       };
@@ -116,56 +105,16 @@ async function sendRequest(userRequest, newlyCreatedRequestId, res) {
   } catch (err) {
     console.log(err);
   }
-
-  // axios(options)
-  // .then(function (responseData) {
-  //   console.log("4. Server receives response from that API call", Date.now())
-  //   insertRawRequestResponse(responseData, newlyCreatedRequestId);
-  //   res.status(200).send("OK");
-  //   })
-    // .then(function () {
-    //   console.log("8. Sends back response to the Frontend", Date.now())
-    //   res.status(200).send("OK");
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // });
 }
 
 async function insertRawRequestResponse(rawResponse, newlyCreatedRequestId) {
   let {rawRequest, parsedResponse} = buildRequestResponse(rawResponse);
 
   rawResponse = String(rawResponse);
-
-  // console.log("5. Inserts the response data into the database", Date.now());
-
   await dbquery(
     `UPDATE requests SET raw_request=$2, raw_response=$3, parsed_response=$4 WHERE id=$1`,
     [newlyCreatedRequestId, rawRequest, rawResponse, parsedResponse]
   );
-  // console.log("6. Database updated", Date.now());
-}
-
-function formatQueryData(data) {
-  return data.map((request) => {
-    let rawRequest = request.raw_request || "";
-    let parsedResponse = request.parsed_response || {};
-
-    return {
-      id: request.id,
-      httpVerb: request.user_request.httpVerb,
-      hostpath: request.user_request.hostpath,
-      time: request.user_request.time,
-      timeZone: request.user_request.timeZone,
-      date: request.user_request.date,
-      name: request.user_request.name,
-      headers: request.user_request.headers,
-      parameters: request.user_request.parameters,
-      body: request.user_request.body,
-      request: rawRequest,
-      response: parsedResponse,
-    };
-  });
 }
 
 // Render React App
@@ -177,8 +126,7 @@ app.get("/", async (req, res) => {
 // Returns a list of ALL request
 app.get("/allrequests", async (req, res, next) => {
   try {
-    let allData = await dbquery("SELECT * FROM requests");
-    let allRequests = formatQueryData(allData.rows);
+    let allRequests = await res.locals.user.getAllData()
 
     res.status(200).send(JSON.stringify(allRequests));
   } catch (err) {
@@ -246,24 +194,11 @@ app.post("/login", async (req, res, next) => {
 // Send the request received from user (either now or later)
 app.post("/makerequest", async (req, res, next) => {
   try { 
-    console.log(req.body, 'line 190');
     let userRequest = req.body;
-
-    console.log(userRequest, 'line 192');
     let timeScheduled = createTimeScheduled(userRequest);
+    let newlyCreatedRequestId = await res.locals.user.insertRequest(userRequest, timeScheduled);
 
-    console.log(timeScheduled, 'line 193');
-    // console.log("2. Server inserts user request into database", Date.now());
-    let queryResult = await dbquery(
-      `INSERT INTO requests (user_id, user_request, time_scheduled) VALUES ($1, $2, $3) RETURNING id`,
-      [1, userRequest, timeScheduled]
-    );
-
-    let newlyCreatedRequestId = queryResult.rows[0].id;
-
-    if (queryResult.rowCount > 0) {
-      sendRequest(userRequest, newlyCreatedRequestId, res);
-    }
+    await sendRequest(userRequest, newlyCreatedRequestId, res);
   } catch (err) {
     next(err);
   }
